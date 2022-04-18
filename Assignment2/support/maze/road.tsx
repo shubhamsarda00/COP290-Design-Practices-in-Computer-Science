43a8d8fee012aa3c4d73dc9e29e1289fd89b0612
shubhamsarda00 <45795080+shubhamsarda00@users.noreply.github.com>
2022-04-19 01:17:18 +0530

    Delete Assignment2 directory

--- Assignment2/support/maze/road.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="road" tilewidth="32" tileheight="32" tilecount="45" columns="5">
- <image source="road.jpg" width="168" height="300"/>
-</tileset>
--- Assignment2/support/maze/terrain.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="terrain" tilewidth="32" tileheight="32" tilecount="49" columns="7">
- <image source="terrain.jpg" width="225" height="225"/>
-</tileset>
