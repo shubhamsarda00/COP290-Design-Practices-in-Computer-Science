04042917abe8ab77ffe0fb0db7626b4c1b531f6d
shubhamsarda00 <45795080+shubhamsarda00@users.noreply.github.com>
2022-06-05 00:10:36 +0530

    Delete Assignment2 directory

--- Assignment2/support/maze/h.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="h" tilewidth="1" tileheight="1" tilecount="0" columns="0" objectalignment="topleft">
- <grid orientation="orthogonal" width="60" height="60"/>
-</tileset>
--- Assignment2/support/maze/water.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="water" tilewidth="32" tileheight="32" tilecount="49" columns="7">
- <image source="water.jpg" width="225" height="225"/>
-</tileset>
