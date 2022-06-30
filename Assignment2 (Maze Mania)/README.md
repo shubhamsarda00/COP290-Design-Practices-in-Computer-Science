03d6434facea5288e24fc54fe6893100b69d761e
Shubham Sarda <shubhamsarda00@gmail.com>
2022-07-01 01:19:32 +0530

    Add files via upload

---
+++ Assignment2 (Maze Mania)/README.md
@@ -0,0 +1,23 @@
+# Maze Mania
+This is a 2-player maze arcade game developed as a part of the course COP-290. Wi-Fi is used as the medium connecting server and client.
+
+Installation Instructions:
+
+1. Download/clone the complete git repository.
+2. In the assignment 2 directory, open termninal and run: $make
+3. In the same terminal, run $ ./game to begin the game!
+
+
+Library Requirements:
+
+1. SDL2:  sudo apt-get install libsdl2-dev
+2. SDL2-Image:  sudo apt-get install libsd2l-image-dev
+3. SDL2-TTF:  sudo apt-get install libsdl2-ttf-dev
+4. SDL2-Mixer:  sudo apt-get install libsdl2-mixer-dev
+
+OS Required: Ubuntu (preferably verion 20.04)
+
+Online Resources for learning game developement using SDL2 in C++:
+1. https://lazyfoo.net/tutorials/SDL/
+2. https://www.youtube.com/playlist?list=PLVotA8ycjnCs3DNWIbEIpBrjYkhJq11q-
+3. Special Mentions: stackoverflow.com
---
+++ Assignment2 (Maze Mania)/src/menu.h
@@ -0,0 +1,72 @@
+/*
+ * menu.h
+ *
+ *  Created on: 02-Apr-2022
+ *      Author: shubham
+ */
+
+#ifndef ASSIGNMENT2_MENU_H_
+#define ASSIGNMENT2_MENU_H_
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+//#include <math.h>
+#include <vector>
+#include <string.h>
+//#include <fstream>
+#include <iostream>
+#include "music.h"
+using namespace std;
+
+#define MENU_WIDTH 800
+#define MENU_HEIGHT 800
+
+class Menu{
+
+	public:
+//	SDL_Window* window=NULL;
+//	SDL_Renderer* sdlr=NULL;
+//	SDL_Event event;
+	SDL_Texture* currentTexture;
+	int mousex,mousey,highscore;
+	vector<SDL_Rect> buttons;
+	SDL_Texture* texture;
+	SDL_Texture* texture1;
+	SDL_Texture* texture2;
+	SDL_Texture* texture3;
+	SDL_Texture* texture4;
+	SDL_Texture* texture5;
+	SDL_Texture* instructions;
+	SDL_Texture* winscreen;
+	SDL_Texture* nameScreen;
+	TTF_Font* font;
+	Music music;
+	string p1Name="";
+	string p1Hostel="ARAVALI";
+	string ip="192.168.16.3";
+	string isClient="YES";
+	string time="15";
+	bool isOpen,fullscreen,playMusic,isPlaying;
+//	vector<string> options;
+	Menu();
+	~Menu();
+	void initialize();
+	void loadMenu(SDL_Renderer* sdlr);
+	void toggleMusic();
+	bool render(SDL_Event event,SDL_Window* window,SDL_Renderer* sdlr);
+	void write(SDL_Renderer*sdlr,string text,SDL_Color colour,int x, int y);
+	void playSoundEffect(string name,bool delay);
+	void updateFont(string path,int fontSize);
+	void reloadHighScore();
+	void updateHighScore(int score);
+	int getHighScore(){return this->highscore;};
+	bool isInteger(string s);
+	private:
+};
+
+
+
+
+#endif /* ASSIGNMENT2_MENU_H_ */
